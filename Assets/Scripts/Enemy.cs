bb312cace6f3ebd3270e10e8d01e2fe248b9811e
Pablo784 <77360636+Pablo784@users.noreply.github.com>
2022-05-10 21:40:00 -0400

    I Have The Basics...For Now At Least

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ THIS YOUTUBE VIDEO HELPED ME A LOT
+- https://youtu.be/anHxFtiVuiE
+ */
+
+
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health, maxHealth = 3f;
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+
+
+        health -= damageAmount;
+
+        if (health <= 0)
+
+            Destroy(gameObject);
+    }
+
+}
+
---
+++ Assets/Scripts/Space_Ship_Movement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Space_Ship_Movement : MonoBehaviour
+{
+
+    public float speed = 4.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float yMove = Input.GetAxisRaw("Vertical");
+
+        float xMovement = xMove * speed * Time.deltaTime;
+        float yMovement = yMove * speed * Time.deltaTime;
+
+        transform.Translate(xMovement, yMovement, 0);
+
+        // Debug.Log("You Are Moving");
+    }
+}
