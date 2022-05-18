ea38523405ce667a37f8ca2eef7d7478fbc8e900
Pablo784 <77360636+Pablo784@users.noreply.github.com>
2022-05-18 15:25:48 -0400

    Making Some Small Changes

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -29,7 +29,7 @@ public class Score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+
         scoreText.text = "Score :" + (int)scoreAmount; 
         scoreAmount += pointIncreasedPerSecond * Time.deltaTime;
     }
